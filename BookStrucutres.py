c86aacd3462fdff9a120e96efc18525db05aac44
ChandelVatsal <68132483+ChandelVatsal@users.noreply.github.com>
2022-12-22 20:09:48 -0800

    More linkedList stuff

--- BookStrucutres.py
+++ BookStrucutres.py
@@ -3,9 +3,9 @@ from myStructures import *  # For LinkedList
 
 
 def main():
-    chapter1()
+    #chapter1()
     
-    #chapter2()
+    chapter2()
     
     #chapter3()
     
@@ -90,9 +90,10 @@ def palindromePermutation(str):
         elif letter_dict[k] % 2 != 0 and odd != 0:
             return False
     return True
-     
+
+
 def oneAway(str1, str2):
-    #Remove, replace, insert
+    #Remove, rephhhlace, insert
     if str1 == str2:
         return True
     
@@ -104,8 +105,10 @@ def oneAway(str1, str2):
                 if changed:
                     return False
                 changed = True 
-        return True 
+        return True
+    
     elif (len(str1) + 1 == len(str2)) or (len(str1) -1 == len(str2)):
+        
         i,j = 0,0
         changed = False
         while i < len(str1) and j < len(str2):
@@ -117,6 +120,7 @@ def oneAway(str1, str2):
                 i += 1
                 j += 1
         return True
+    
     else:
         return False
                 
@@ -170,6 +174,19 @@ def chapter2():
     
     print()
     
+    print("Delete Middle Node")
+    ll.insert(4)
+    ll.insert(5)
+    
+    ll.printLL()
+
+
+    delMidNode(ll)
+    print()
+    ll.printLL()
+    
+    print()
+    
     print("Adding LinkedList")
     ll1 = LinkedList()
     ll1.insert(5)
@@ -184,6 +201,17 @@ def chapter2():
     
     print()
     
+    print("LinkedList Palindrome")
+    ll4 = LinkedList()
+    ll4.insert(1)
+    ll4.insert(2)
+    ll4.insert(2)
+    ll4.insert(1)
+    print(palindrome(ll4))
+    print(palindrome(ll1))
+    
+    print()
+    
     print("Trying to detect Loop in List")
     ll3 = LinkedList()
     ll3.insert(3)
@@ -192,6 +220,11 @@ def chapter2():
     ll3.insert(12)
     ll3.head.next = ll3.head # creates loop in LinkedList
     print(loopDetection(ll3))
+    
+    print()
+    
+
+    
     
 def removeDups(self):
     current = self.head 
@@ -218,7 +251,26 @@ def kthLast(self, k):
         for i in range(counter - k):
             temp1 = temp1.next
         print(temp1.data)
+
+def delMidNode(self):
+    counter = 0
+    temp = self.head
+
+    while temp is not None:
+        counter += 1
+        temp = temp.next 
+        
+    mid = counter // 2
     
+    while mid > 1:
+        mid -= 1
+        self.head.next = self.head.next
+        
+    self.head.next.next = self.head.next.next.next
+    return self
+        
+        
+  
 def sumLists(ll1, ll2):
     finalSum = 0
     
@@ -243,6 +295,15 @@ def sumLists(ll1, ll2):
         finalList.insert(digit)
         finalSum = finalSum // 10 
     finalList.printLL()
+       
+def palindrome(l1):
+    temp = l1.head
+    number = ""
+    while temp: 
+        number = number + str(temp.data)
+        temp = temp.next
+    return number == number[::-1]        
+            
         
 def loopDetection(self):
     temp = self.head
